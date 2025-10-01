c9f04f9c2cf6218c40fe0878bb393fb60721ebed
nammalwar2000 <nammalwarsowmithri23@gmail.com>
2025-10-01 15:21:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,8 @@ if ingredients_list:
 
     
         st. subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ search_on)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ search_on)
+        sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     
 
